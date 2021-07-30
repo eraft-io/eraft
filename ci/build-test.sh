4bb28715c25f02594395b5b52ccdb82f482c44c8
jayliujie <jay_jieliu@outlook.com>
2021-07-30 21:47:29 +0800

    del unuse ci

--- ci/build-test.sh
+++ ci/build-test.sh
@@ -8,11 +8,11 @@ NPROC=$(nproc || grep -c ^processor /proc/cpuinfo)
 
 # cd "$SRCPATH/rocksdb" && make static_lib && cd -
 
-# if [ -d "$SRCPATH/Protocol" ]; then
-#   cd "$SRCPATH/Protocol"
-#   ./scripts/generate_cpp.sh
-#   cd -
-# fi
+if [ -d "$SRCPATH/Protocol" ]; then
+  cd "$SRCPATH/Protocol"
+  ./scripts/generate_cpp.sh
+  cd -
+fi
 
 build_dir="$SRCPATH/build_"
 mkdir -p $build_dir && cd $build_dir
@@ -25,11 +25,5 @@ if [ ! -d "$SRCPATH/output" ]; then
   mkdir $SRCPATH/output/logs
 fi
 
-cp $build_dir/Logger/test/LoggerTests $SRCPATH/output
 cp $build_dir/RaftCore/test/RaftTests $SRCPATH/output
-cp $build_dir/Kv/test_svr/TestsSvr $SRCPATH/output
 
-# run test
-# $build_dir/Logger/test/LoggerTests
-# $build_dir/RaftCore/test/RaftTests
-# $build_dir/Kv/test_svr/TestsSvr
