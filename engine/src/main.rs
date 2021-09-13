7c49e15ef597e13dd46f6ce39a73a59a07b5a172
jayliujie <jay_jieliu@outlook.com>
2021-09-13 09:26:32 +0800

    add rust engine

---
+++ engine/src/main.rs
@@ -0,0 +1,8 @@
+//! simplekv.rs -- a rust implementation of [simplekv example] from PMDK,
+//! libpmemobj++ of simple kv which uses vector to hold values, string as a key
+//! and array to hold buckets.
+//!
+//! [simplekv example]: https://github.com/pmem/libpmemobj-cpp/tree/master/examples/simplekv
+#![feature(const_fn)]
+
+pub fn main() {}
