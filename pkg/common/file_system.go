2fdf615cdb0f5977e56a82e23fab57250a6e313e
LLiuJJ <jay_jieliu@outlook.com>
2022-07-03 14:59:55 +0800

    add some funcs

---
+++ pkg/common/file_system.go
@@ -0,0 +1,10 @@
+package common
+
+//
+// CalFileCheckSum
+// calculate a file md5 checksum
+//
+func CalFileCheckSum(path string) string {
+
+	return ""
+}
---
+++ pkg/common/slot.go
@@ -0,0 +1,13 @@
+package common
+
+const SLOT_NUM = 1024
+
+//
+// calculate payload hash slot
+// @param payload random
+// @return uint16  slot value (0~1023)
+//
+func StrToSlot(payload string) uint16 {
+
+	return 0
+}
