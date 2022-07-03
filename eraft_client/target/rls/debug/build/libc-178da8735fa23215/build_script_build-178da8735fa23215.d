624156ec06dd4917d4439886dd902f82d0f08950
LLiuJJ <jay_jieliu@outlook.com>
2022-07-03 11:42:59 +0800

    add ignore

--- eraft_client/target/rls/debug/build/libc-178da8735fa23215/build_script_build-178da8735fa23215.d
+++
@@ -1,5 +0,0 @@
-/Users/colin/Documents/eraft/eraft_client/target/rls/debug/build/libc-178da8735fa23215/build_script_build-178da8735fa23215: /Users/colin/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/libc-0.2.126/build.rs
-
-/Users/colin/Documents/eraft/eraft_client/target/rls/debug/build/libc-178da8735fa23215/build_script_build-178da8735fa23215.d: /Users/colin/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/libc-0.2.126/build.rs
-
-/Users/colin/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/libc-0.2.126/build.rs:
