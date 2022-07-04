67a2bddf620b01566415ee4d3d317c77f1e0af77
LLiuJJ <jay_jieliu@outlook.com>
2022-07-04 21:54:17 +0800

    add copyright

--- pkg/common/slot.go
+++ pkg/common/slot.go
@@ -1,6 +1,20 @@
+// Copyright [2022] [WellWood] [wellwood-x@googlegroups.com]
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package common
 
-const SLOT_NUM = 1024
+import "github.com/eraft-io/eraft/pkg/consts"
 
 //
 // calculate payload hash slot
@@ -8,5 +22,5 @@ const SLOT_NUM = 1024
 // @return uint16  slot value (0~1023)
 //
 func StrToSlot(payload string) uint64 {
-	return XXH3_64bits([]byte(payload)) % 1024
+	return XXH3_64bits([]byte(payload)) % consts.SLOT_NUM
 }
--- test/common/slot_test.go
+++ test/common/slot_test.go
@@ -1,11 +1,26 @@
+// Copyright [2022] [WellWood] [wellwood-x@googlegroups.com]
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package common
 
 import (
-	cm "github.com/eraft-io/eraft/pkg/common"
 	"math/rand"
 	"strings"
 	"testing"
 	"time"
+
+	cm "github.com/eraft-io/eraft/pkg/common"
 )
 
 func getRandstring(length int) string {
